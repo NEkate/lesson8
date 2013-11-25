5256b747be8881e890d822610cda2afaab246c5d
Kate <katren4@yandex.ru>
2013-11-25 20:20:46 +0200

    присвоение значения свойству

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 ;(function($){
 
-    $.fn.asd = function(){
-        console.log(this);
-        return this;// дает возможность вызывать другие мтоды через точку
-    }
-     $('script').asd();
+    var scripts = $('input');
+    console.log(scripts.prop({value: 'Kate', title: 'Name'}));
+//    console.log(scripts[0].value.length);
+//    console.log(scripts.eq(1).attr('type'));
 })(jQuery);
