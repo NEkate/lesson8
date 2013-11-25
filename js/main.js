82138c12d656a7770474590fda7c35fbdf145af0
Kate <katren4@yandex.ru>
2013-11-25 20:43:31 +0200

    добавляем инпут в первое найденное боди ипосле добавляем ему велью

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 ;(function($){
 
-    var inputs = $('input');
-    console.log(inputs.prop({value: 'Kate', title: 'Name'}));
-    console.log(inputs.prop('title',function(index, value){
-        console.log('this:', this.value);
-        console.log('arguments:', arguments);
-    }));
-//    console.log(scripts[0].value.length);
-//    console.log(scripts.eq(1).attr('type'));
+    $('<input type="password">').prop({
+        value: 'Kate'
+    }).appendTo('body');
+
 })(jQuery);
