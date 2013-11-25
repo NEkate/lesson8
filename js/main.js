aff873eaa040c39df3b91f783dc16d8103f38883
Kate <katren4@yandex.ru>
2013-11-25 20:29:51 +0200

    this и  arguments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 ;(function($){
 
-    var scripts = $('input');
-    console.log(scripts.prop({value: 'Kate', title: 'Name'}));
+    var inputs = $('input');
+    console.log(inputs.prop({value: 'Kate', title: 'Name'}));
+    console.log(inputs.prop('title',function(index, value){
+        console.log('this:', this.value);
+        console.log('arguments:', arguments);
+    }));
 //    console.log(scripts[0].value.length);
 //    console.log(scripts.eq(1).attr('type'));
 })(jQuery);
